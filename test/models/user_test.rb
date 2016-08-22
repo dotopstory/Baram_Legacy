4df4d67f6fe866db891a8854a1dccc7b040e7bfd
Charles Oh <mun5424@gmail.com>
2016-08-22 16:28:50 +0000

    Add user following module

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,32 @@ class UserTest < ActiveSupport::TestCase
     end 
   end 
   
+  test "should follow and unfollow a user" do 
+    charlie = users(:charlie) 
+    archer = users(:archer) 
+    assert_not charlie.following?(archer) 
+    charlie.follow(archer) 
+    assert charlie.following?(archer) 
+    charlie.unfollow(archer)
+    assert_not charlie.following?(archer)
+  end 
+  
+  test "feed should have the right posts" do
+    charlie = users(:charlie)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert charlie.feed.include?(post_following)
+    end
+    # Posts from self
+    charlie.microposts.each do |post_self|
+      assert charlie.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not charlie.feed.include?(post_unfollowed)
+    end
+  end 
+
 end
