525535892e0f0ae5962790b4e1bd6bd5d6c1c6bc
Charles Oh <mun5424@gmail.com>
2017-02-08 18:19:39 +0000

    seed data from sqlite into postgresql

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,13 @@
-User.create!(name: "Example User", 
-            email: "example@railstutorial.org" ,
-            password: "foobar" ,
-            password_confirmation: "foobar" ,
+User.create!(name: "baram", 
+            email: "baram@baramlegacy.com" ,
+            password: "123123" ,
+            password_confirmation: "123123" ,
             admin: true, 
             activated: true,
             activated_at: Time.zone.now)
 
-            
-            
-99.times do |n| 
-    name = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org" 
-    password = "password" 
-    User.create!(name: name,
-                email: email, 
-                password: password,
-                password_confirmation: password,
-                activated: true,
-                activated_at: Time.zone.now)
-end 
+
+Monster.import("/app/assets/images/monsters/monsters.xlsx")
+Item.import("/app/assets/images/monsters/monsters.xlsx")
+ 
             
