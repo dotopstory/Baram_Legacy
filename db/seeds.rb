52d28c1eb6603023857c5c4a0ba6da81a154f875
Charles Oh <mun5424@gmail.com>
2017-02-08 18:33:27 +0000

    take out unnecessary images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
-User.create!(name: "baram", 
+require 'rubygems'
+require 'roo'
+
+unless User.find_by( email: "baram@baramlegacy.com" )
+    User.create!(name: "baram", 
             email: "baram@baramlegacy.com" ,
             password: "123123" ,
             password_confirmation: "123123" ,
             admin: true, 
             activated: true,
             activated_at: Time.zone.now)
+end
 
 
 Monster.import("/app/assets/images/monsters/monsters.xlsx")
-Item.import("/app/assets/images/monsters/monsters.xlsx")
+
+Item.import("/app/assets/images/monsters/items.xlsx")
  
             
