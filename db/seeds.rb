3739eac1d396b53842033643130b432da14bf697
Charles Oh <mun5424@gmail.com>
2017-02-08 18:49:15 +0000

    Items should be inserted before monsters

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,9 @@ unless User.find_by( email: "baram@baramlegacy.com" )
 end
 
 
+Item.import("https://s3-us-west-1.amazonaws.com/baramlegacy/images/Data/items.xlsx")
+
 Monster.import("https://s3-us-west-1.amazonaws.com/baramlegacy/images/Data/monsters.xlsx")
 
-Item.import("https://s3-us-west-1.amazonaws.com/baramlegacy/images/Data/items.xlsx")
  
             
